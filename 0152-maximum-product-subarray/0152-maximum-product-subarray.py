71f9c5ef5cd7eedfb5ecbe3301057f06d6dfd218
Abdullah M <abuofficial735@gmail.com>
2026-08-22 09:42:05 +0530

    Time: 15 ms (17%), Space: 13.2 MB (31.94%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.py
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.py
@@ -1,21 +1,12 @@
 class Solution(object):
     def maxProduct(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
         current_max = nums[0]
         current_min = nums[0]
         result = nums[0]
-
         for i in range(1, len(nums)):
-
             if nums[i] < 0:
                 current_max, current_min = current_min, current_max
-
             current_max = max(nums[i], current_max * nums[i])
             current_min = min(nums[i], current_min * nums[i])
-
             result = max(result, current_max)
-
         return result
