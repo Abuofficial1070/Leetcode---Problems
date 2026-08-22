a645e64cd75ef38414eca5bbbfcedef6af8fb0c4
Abdullah M <abuofficial735@gmail.com>
2026-08-22 11:19:01 +0530

    Time: 9 ms (29.08%), Space: 14.4 MB (19.68%) - LeetHub

---
+++ 0136-single-number/0136-single-number.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        s={}
+        for i in nums:
+            if i in s:
+                s[i]+=1
+            else:
+                s[i]=1
+        for i in nums:
+            if s[i]==1:
+                return i
